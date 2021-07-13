9b21d4d0bbfb21313d48e8eea8d6546f54b7457c
fujita <msg.1801026@gmail.cojm>
2021-07-13 09:40:26 +0900

    時間割のダウンロード機能の追加

---
+++ js/timetable_save.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () => {
+  const button1 = document.getElementById('button1');
+  button1.addEventListener('click', button1_clicked);
+});
+
+function button1_clicked(evt) {
+  evt.preventDefault();
+  var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+  var content = '';
+
+  for (let z = 0; z < timetable.length; z++) {
+    for (let y = 0; y < timetable[z].length; y++) {
+      for (let x = 0; x < timetable[z][y].length; x++) {
+        let tmp = get_lesson_status(z, timetable[z][y][x]);
+        tmp = trans_after_lesson([z, timetable[z][y][x], tmp[1], tmp[2]]);
+        tmp = '"' +
+          tmp[0] + '\n' +
+          tmp[1] + '\n' +
+          tmp[2] + '"';
+        content += tmp + ',';
+      }
+      content += '\n';
+    }
+    content += '\n';
+  }
+
+  var blob = new Blob([bom, content], { "type": "text/csv" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.download = '時間割.csv';
+  a.href = url;
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
+
