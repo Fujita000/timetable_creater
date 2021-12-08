d5bc0395d0cce1c3b6cf8fa663155c076cf9ef27
藤田 <msg.1801026@gmail.com>
2021-12-08 23:34:57 +0900

    自動生成後データの読み込み後自動生成使用時のバグ処理

--- js/auto_create/auto_create.js
+++ js/auto_create/auto_create.js
@@ -2,21 +2,22 @@
 let run_initial, lesson_initial, au_choice_lesson, pass_timetable, pass_lesson;
 // let run_initial = JSON.parse(JSON.stringify(pass_timetable));
 // let lesson_initial = JSON.parse(JSON.stringify(pass_lesson));
-let tmp_timetable, auto_create_flag = false;
-document.querySelector("#auto_create_cancel").addEventListener("click", e => {
+let tmp_timetable,
+  auto_create_flag = false;
+document.querySelector("#auto_create_cancel").addEventListener("click", (e) => {
   if (auto_create_flag) {
     timetable = JSON.parse(JSON.stringify(tmp_timetable));
-    update_timetable()
-    all_color_change()
+    update_timetable();
+    all_color_change();
     auto_create_flag = false;
     tmp_timetable = undefined;
   }
 });
-document.querySelector("#auto_create").addEventListener("click", e => {
+document.querySelector("#auto_create").addEventListener("click", (e) => {
   if (!auto_create_flag) tmp_timetable = JSON.parse(JSON.stringify(timetable));
   click_auto_create();
   auto_create_flag = true;
-})
+});
 
 function click_auto_create() {
   //開始ボタンを押したとき
@@ -26,24 +27,26 @@ function click_auto_create() {
   lesson_initial = JSON.parse(JSON.stringify(tps[1]));
   au_choice_lesson = JSON.parse(JSON.stringify(tps[2]));
   initialization();
+  let _run_timetable = run_timetable();
+  let rt = _run_timetable[0];
+  let err = _run_timetable[1];
+  timetable = lesson_out_timetable(rev_y_x(JSON.parse(JSON.stringify(rt))));
+  if (err) alert("エラーが発生しました\n\n" + count_lesson_list());
 
-  let rt = run_timetable()
-  timetable = lesson_out_timetable(rev_y_x(JSON.parse(JSON.stringify(rt))))
-
-  update_timetable()
-  all_color_change()
+  update_timetable();
+  all_color_change();
 }
 
 function validate_timetable(timetable) {
   let tt = shallow_copy(timetable);
   //timetableの中にある授業が現在存在するかの確認
   //ない場合は削除する
-  tt.forEach(arr => {
-    arr.forEach(table => {
-      table.forEach(data => {
+  tt.forEach((arr) => {
+    arr.forEach((table) => {
+      table.forEach((data) => {
         console.log(data);
       });
-    })
+    });
   });
 }
 
@@ -73,7 +76,7 @@ var vacant_timetable = (v_timetable, v_grade, v_weekday, v_time) => {
     return [v_weekday, v_time];
   }
   return [999, 999];
-}
+};
 
 // 空いている時間が何時間空いているか探す関数
 var continuous = (c_timetable, c_grade, c_weekday, c_time) => {
@@ -85,7 +88,7 @@ var continuous = (c_timetable, c_grade, c_weekday, c_time) => {
     temp++;
   }
   return temp;
-}
+};
 
 //空いている教室を見つける関数
 var find_class = (f_timetable, f_weekday, f_time) => {
@@ -107,7 +110,7 @@ var find_class = (f_timetable, f_weekday, f_time) => {
     f_class.push(f_timetable[i][f_weekday][f_time][2]);
   }
   return f_class;
-}
+};
 
 //空いている先生を見つける関数
 var find_teacher = (f_timetable, f_weekday, f_time) => {
@@ -129,10 +132,11 @@ var find_teacher = (f_timetable, f_weekday, f_time) => {
     f_teacher.push(f_timetable[i][f_weekday][f_time][1]);
   }
   return f_teacher;
-}
+};
 
 //空いている先生と教室がマッチする授業を見つける
-var match = (m_teacher, m_class, m_grade, m_continuous) => {    //この引数(m_teacher,m_class)には配列が入ります。
+var match = (m_teacher, m_class, m_grade, m_continuous) => {
+  //この引数(m_teacher,m_class)には配列が入ります。
   let match_class = [];
   let now_lesson;
   for (i = 0; i < pass_lesson[m_grade].length; i++) {
@@ -141,13 +145,17 @@ var match = (m_teacher, m_class, m_grade, m_continuous) => {    //この引数(m
     }
     let not_match = false;
     for (j = 0; j < m_teacher.length; j++) {
-      if (pass_lesson[m_grade][i][1] == m_teacher[j] && pass_lesson[m_grade][i][1] != 0) {
+      if (
+        pass_lesson[m_grade][i][1] == m_teacher[j] &&
+        pass_lesson[m_grade][i][1] != 0
+      ) {
         not_match = true;
         break;
-      } else if (pass_lesson[m_grade][i][1] == 0) {  //変える予定
+      } else if (pass_lesson[m_grade][i][1] == 0) {
+        //変える予定
         now_lesson = pass_lesson[m_grade][i][0];
         for (k = 0; k < au_choice_lesson[m_grade].length; k++) {
-          if (now_lesson = !au_choice_lesson[m_grade][k][0]) {
+          if ((now_lesson = !au_choice_lesson[m_grade][k][0])) {
             continue;
           }
           // if (au_choice_lesson[i] === void 0) {
@@ -161,13 +169,17 @@ var match = (m_teacher, m_class, m_grade, m_continuous) => {    //この引数(m
     }
     if (not_match == false) {
       for (j = 0; j < m_class.length; j++) {
-        if (pass_lesson[m_grade][i][2] == m_class[j] && pass_lesson[m_grade][i][2] != 0) {
+        if (
+          pass_lesson[m_grade][i][2] == m_class[j] &&
+          pass_lesson[m_grade][i][2] != 0
+        ) {
           not_match = true;
           break;
-        } else if (pass_lesson[m_grade][i][1] == 0) {  //変える予定
+        } else if (pass_lesson[m_grade][i][1] == 0) {
+          //変える予定
           now_lesson = pass_lesson[m_grade][i][0];
           for (k = 0; k < au_choice_lesson[m_grade].length; k++) {
-            if (now_lesson = !au_choice_lesson[m_grade][k][0]) {
+            if ((now_lesson = !au_choice_lesson[m_grade][k][0])) {
               continue;
             }
             // if (au_choice_lesson[i] === void 0) {
@@ -190,7 +202,7 @@ var match = (m_teacher, m_class, m_grade, m_continuous) => {    //この引数(m
     }
   }
   return match_class;
-}
+};
 
 // 空いている連続時間に授業を入れる関数
 var put_class = (p_grade, weekday, time, p_class, p_continuous) => {
@@ -201,7 +213,7 @@ var put_class = (p_grade, weekday, time, p_class, p_continuous) => {
     pass_timetable[p_grade][weekday][time + i] = pass_lesson[p_grade][p_class];
     pass_lesson[p_grade][p_class][4]++;
   }
-}
+};
 
 function run_timetable() {
   let run_vacant = [];
@@ -212,9 +224,18 @@ function run_timetable() {
   let run_count = 0;
   let random = Math.floor(Math.random() * 169);
   let exit_count = 0;
+  let err = false;
   loop: for (c = 0; c < pass_timetable.length; c++) {
-    for (run_weekday = 0; run_weekday < pass_timetable[c].length; run_weekday++) {
-      for (run_time = 0; run_time < pass_timetable[c][run_weekday].length; run_time++) {
+    for (
+      run_weekday = 0;
+      run_weekday < pass_timetable[c].length;
+      run_weekday++
+    ) {
+      for (
+        run_time = 0;
+        run_time < pass_timetable[c][run_weekday].length;
+        run_time++
+      ) {
         run_vacant = [];
         run_continuous = 0;
         run_find_class = [];
@@ -226,23 +247,56 @@ function run_timetable() {
         if (run_vacant[0] == 999) {
           continue;
         }
-        run_continuous = continuous(pass_timetable, c, run_vacant[0], run_vacant[1]);
+        run_continuous = continuous(
+          pass_timetable,
+          c,
+          run_vacant[0],
+          run_vacant[1]
+        );
         for (f = 0; f < run_continuous; f++) {
-          run_find_class[f] = find_class(pass_timetable, run_vacant[0], run_vacant[1] + f, c);
-          run_find_teacher[f] = find_teacher(pass_timetable, run_vacant[0], run_vacant[1] + f, c);
+          run_find_class[f] = find_class(
+            pass_timetable,
+            run_vacant[0],
+            run_vacant[1] + f,
+            c
+          );
+          run_find_teacher[f] = find_teacher(
+            pass_timetable,
+            run_vacant[0],
+            run_vacant[1] + f,
+            c
+          );
         }
-        run_match_class = run_find_class.reduce((pre, current) => { pre.push(...current); return pre }, []);
-        run_match_teacher = run_find_teacher.reduce((pre, current) => { pre.push(...current); return pre }, []);
-        run_match = match(run_match_teacher, run_match_class, c, run_continuous);
+        run_match_class = run_find_class.reduce((pre, current) => {
+          pre.push(...current);
+          return pre;
+        }, []);
+        run_match_teacher = run_find_teacher.reduce((pre, current) => {
+          pre.push(...current);
+          return pre;
+        }, []);
+        run_match = match(
+          run_match_teacher,
+          run_match_class,
+          c,
+          run_continuous
+        );
         if (!run_match.length) {
           continue;
         }
-        put_class(c, run_vacant[0], run_vacant[1], run_match[(run_count + random) % run_match.length], run_continuous);
+        put_class(
+          c,
+          run_vacant[0],
+          run_vacant[1],
+          run_match[(run_count + random) % run_match.length],
+          run_continuous
+        );
       }
     }
     if (run_count >= pass_lesson[c].length) {
       exit_count++;
       if (exit_count > 100) {
+        err = true;
         break;
       }
       run_count = 0;
@@ -264,7 +318,7 @@ function run_timetable() {
       }
     }
   }
-  return pass_timetable;
+  return [pass_timetable, err];
 }
 
 function initialization() {
@@ -272,34 +326,33 @@ function initialization() {
   pass_lesson = JSON.parse(JSON.stringify(lesson_initial));
 }
 
-function count_lesson_list(){
-  const lsn_list = join_lesson_and_fixed()
-  let cls_name = "";
-  const lsn_name = "";
-  const diff = 0;
+function count_lesson_list() {
+  const lsn_list = join_lesson_and_fixed();
   let msg = "";
-  
-  lsn_list.forEach((e,cls)=>{
+
+  lsn_list.forEach((e, cls) => {
     let cls_name = class_list[cls];
     let flag = true;
-    e.forEach((i)=>{
-      if(i[3] - i[4] != 0){
-        if(flag){
+    e.forEach((i) => {
+      if (i[3] - i[4] != 0) {
+        if (flag) {
           msg += cls_name + "\n";
           flag = false;
         }
-        msg += `${get_lesson_name(cls, i[0])}が${(i[3] - i[4])}${((i[3] - i[4]　> 0) ? "つ足りません" : "つ多く入っています" )}\n`;
+        msg += `${get_lesson_name(cls, i[0])}が${
+          i[3] - i[4] > 0 ? i[3] - i[4] : (i[3] - i[4]) * -1
+        }${i[3] - i[4] > 0 ? "つ足りません" : "つ多く入っています"}\n`;
       }
     });
   });
   return msg;
 }
 
-function get_lesson_name(cls, num){
+function get_lesson_name(cls, num) {
   const names = get_lesson_name_list();
-  if(num >= 100){
-    return "選択授業" + (num - 99)
-  }else{
-    return names[cls][num]
+  if (num >= 100) {
+    return "選択授業" + (num - 99);
+  } else {
+    return names[cls][num];
   }
 }
