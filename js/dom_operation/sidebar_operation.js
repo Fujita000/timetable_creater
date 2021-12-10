2a4bbed13401751adb81f8617fd211ebf662c2fb
fujita <msg.1801026@gmail.com>
2021-12-10 12:31:41 +0900

    教師の使用授業数の追加

--- js/dom_operation/sidebar_operation.js
+++ js/dom_operation/sidebar_operation.js
@@ -27,8 +27,48 @@ function rewrite_name_operation_link_sidebar(suf) {
   operation_link_sidebar.children[suf].innerText = class_list[suf];
 }
 
+function count_number_lesson(list,num){
+  let ret = 0
+  document.querySelectorAll(`.${list}_select`).forEach(i=>{
+    if(i.selectedIndex == num){
+      ret++;
+    }
+  })
+  return ret;
+}
+
+function aaa_teacher(){
+  let ret = []
+  teacher_list.forEach((e,i)=>{
+    ret[i] = 0;
+  })
+
+  join_lesson_and_fixed().forEach((i,cls_num)=>{
+    i.forEach(e=>{
+      if(e[0] < 100){
+        ret[e[1]] += e[3];
+      }else{
+        find_ele(cls_num,e[0]).forEach(j => {
+          ret[j[1]] += e[3];
+        })
+      }
+    })
+  })
+  teacher_list.forEach((e,i)=>{
+    console.log(e,ret[i])
+    
+  })
+  document.querySelectorAll(".teacher_select").forEach(par=>{
+    for (let i = 0; i < par.children.length; i++) {
+      const child = par[i];
+      child.innerText = `${teacher_list[i]} [${ret[i]}]`
+      
+    }
+  })
+  return ret;
+}
 
 
-
-
-
+function find_ele(cls_num,lsn_num){
+  return elective_lesson_list[cls_num][lsn_num - 100];
+}
